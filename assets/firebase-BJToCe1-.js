efc4ebe0622989a81700b1f3487d00aeaaec4599
tberch-ts <tom@talkstudio.space>
2026-08-17 05:16:11 -0600

    Remove Markets/Learn nav items; rebuild frontend

--- assets/firebase-BJToCe1-.js
+++
@@ -1,2 +0,0 @@
-import{f as r,j as t,h as e,m as l,l as p,k as d}from"./index-DXrHmvtz.js";const a=null;export{a as analytics,r as app,t as auth,e as db,l as googleProvider,p as rtdb,d as storage};
-//# sourceMappingURL=firebase-BJToCe1-.js.map
