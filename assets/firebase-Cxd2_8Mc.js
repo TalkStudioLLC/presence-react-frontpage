2a7b7583881ec55f81ee49dc50b3679247098c7a
tberch-ts <tom@talkstudio.space>
2026-08-17 04:58:53 -0600

    Publish talkstudio.space: fix Firebase Storage bucket (firebasestorage.app)

--- assets/firebase-Cxd2_8Mc.js
+++
@@ -1,2 +0,0 @@
-import{f as r,j as t,h as e,m as l,l as p,k as d}from"./index-xM1j1vdu.js";const a=null;export{a as analytics,r as app,t as auth,e as db,l as googleProvider,p as rtdb,d as storage};
-//# sourceMappingURL=firebase-Cxd2_8Mc.js.map
